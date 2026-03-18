19e269a84e3c9ec7ba17eb29579add55a58e1083
Gayan Kaveenda <gayankaveenda@gmail.com>
2026-03-18 15:00:06 +1100

    Added base project into the git

---
+++ src/main/java/au/com/transport/tapngo/TapngoApplication.java
@@ -0,0 +1,13 @@
+package au.com.transport.tapngo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapngoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TapngoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/au/com/transport/tapngo/TapngoApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.transport.tapngo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapngoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
